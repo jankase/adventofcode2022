9563393ca2da440d0a247643c89e16f70b6a8719
Jan Kase <jan.kase@icloud.com>
2022-12-02 00:47:47 +0100

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,41 @@
+import kotlin.math.min
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+  fun calories(input: List<String>): List<Int> {
+    val elfs = input.split({ it.isNotBlank() }, true)
+    return elfs.map { stringCalories -> stringCalories.sumOf { it.toInt() } }
+  }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+  fun part1(input: List<String>): Int {
+    val calories = calories(input)
+    return calories.max()
+  }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+  fun part2(input: List<String>): Int {
+    val calories = calories(input)
+    val top3Calories = calories.sorted().takeLast(3)
+    return top3Calories.sum()
+  }
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+  // test if implementation meets criteria from the description, like:
+  val testInput = readInput("Day01_test")
+  check(part1(testInput) == 24000)
+  check(part2(testInput) == 45000)
+
+  val input = readInput("Day01")
+  println(part1(input))
+  println(part2(input))
+}
+
+
+fun <T> Iterable<T>.split(predicate: (T) -> Boolean, removeSeparator: Boolean): List<List<T>> {
+  var remaining = this.toList()
+  val result = mutableListOf<List<T>>()
+  while (remaining.isNotEmpty()) {
+    val newGroup = remaining.takeWhile(predicate)
+    result.add(newGroup)
+    val fromIndex = newGroup.size + if (removeSeparator) 1 else 0
+    remaining = remaining.subList(min(fromIndex, remaining.size), remaining.size)
+  }
+  return result
 }
